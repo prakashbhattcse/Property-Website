3499696ec70d682bc6eed98cf653d42a18366e8a
prakashbhattcse <bhattprakash5u@gmail.com>
2024-09-16 13:54:43 +0530

    final code

--- Server/middlewares/verifyToken.js
+++ Server/middlewares/verifyToken.js
@@ -1,31 +1,28 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    try {
-        const headerToken = req.headers['authorization'];
+  try {
+    const headerToken = req.headers["authorization"];
 
-        // Check if token exists
-        if (!headerToken) {
-            return res.status(401).json({ message: 'Unauthorized access' });
-        }
-
-        // Directly use the token from the header
-        const token = headerToken; 
-
-        // Verify the token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Attach user ID from token to request object
-        req.userId = decoded.id; 
-
-        next();
-    } catch (error) {
-        console.log(error);
-        res.status(401).json({
-            errorMessage: 'Invalid token!',
-            isTokenExpires: true,
-        });
+    if (!headerToken) {
+      return res.status(401).json({ message: "Unauthorized access" });
     }
+
+    const token = headerToken;
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach user ID from token to request object
+    req.userId = decoded.id;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      errorMessage: "Invalid token!",
+      isTokenExpires: true,
+    });
+  }
 };
 
 module.exports = { verifyToken };
